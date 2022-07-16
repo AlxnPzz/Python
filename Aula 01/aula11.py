8d1bbf08e12fa48c6ce5d9b02dddd40c49b12224
AlxnPzz <109228832+AlxnPzz@users.noreply.github.com>
2022-07-16 16:42:10 -0400

    Add files via upload

---
+++ Aula 01/aula11.py
@@ -0,0 +1,43 @@
+"""
+Operadores lógicos
+and, or, not
+in "Está" e not in "Não está"
+
+
+#(Verdadeira E Verdadeira) = Verdadeiro
+comparacao1 and comparacao2
+
+#(Verdadeira E Falso) = Falso
+comparacao1 and comparacao2
+
+#Verdadeiro OU Verdadeiro = Verdadeiro
+comp1 or com2
+
+#Verdadeiro OU Falso = Verdadeiro
+comp1 or com2
+"""
+a = 2
+b = 3
+if b>a:
+    print("B e maior q A")
+else:
+    print("A é maior q B")
+
+if not b>a:
+    print("B e maior q A")
+else:
+    print("A é maior q B")
+v1='asda'
+v2=1
+
+if not v2:
+    print("Por favor, preencha o valor de A")
+
+nome = 'Alexandre Piazza'
+letra = input("Digite uma letra")
+
+if letra not in nome:
+    print(f'Existe a letra {letra} no seu nome')
+else:
+    print("Não existe essa letra")
+
