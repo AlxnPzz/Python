333544510b871cdd11037394be3993063e235839
AlxnPzz <109228832+AlxnPzz@users.noreply.github.com>
2022-07-16 15:09:08 -0400

    Update aula8.py

--- Aula 01/aula8.py
+++ Aula 01/aula8.py
@@ -5,4 +5,5 @@ Entrada de dados - Input
 nome = input("Qual o seu nome? ")
 idade = input("Qual a sua idade? ")
 ano_nascimento = 2022-int(idade) #aqui eu transformei a idade de str para inteiro
-print(f'Olá {nome} sua idade é {idade} anos, você nasceu em {ano_nascimento}')
+print(f'Olá {nome} sua idade é {idade} anos. '
+      f'Você nasceu em {ano_nascimento}')
