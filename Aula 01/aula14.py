b5e253e5761ab98c8f51aa02eb02a4a7b989bd65
AlxnPzz <109228832+AlxnPzz@users.noreply.github.com>
2022-07-18 16:26:38 -0400

    Add files via upload

---
+++ Aula 01/aula14.py
@@ -0,0 +1,10 @@
+"""
+Pass e Ellipsis "..." como placeholders
+
+"""
+valor = True
+
+if valor:
+    ...
+else:
+    print('Xau')
---
+++ Aula 01/aula15.py
@@ -0,0 +1,38 @@
+"""
+Formatando valores com modificadores
+
+:s - Texto (strings)
+:d - Inteiros (int)
+:f - Número de ponto flutuante (float)
+:.(NUMERO)f - Quantidade de casas decimais (float)
+:(CARACTERE)(> ou < ou ^)(QUANTIDADE)(TIPO - s, d ou f)
+
+> - Esquerda
+< - Direita
+^ - Centro
+"""
+#:f - Número de ponto flutuante (float)
+num_1 = 10
+num_2 = 3
+divisao = num_1 / num_2
+
+print('{:.2f}'.format(divisao))
+print(f'{divisao:.2f} é o resultado')
+
+#:s - Texto (strings)
+
+nome = "Alexandre"
+print(f'{nome:s}')
+
+#:(CARACTERE)(> ou < ou ^)(QUANTIDADE)(TIPO - s, d ou f)
+v1 = 1
+print(f'{v1:0>10}')
+v2 = 15846
+print(f'{v2:0^10}')
+v3 = 15846
+print(f'{v2:.2f}')
+v4 = 15846
+print(f'{v2:0>10.2f}')
+
+nome2 = "Alexandre"
+print(f'{nome:#^50}')
